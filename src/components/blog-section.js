07e451b1680664f0ad93db350e2aa61a9c33c67a
aaron-schuyler <asky28@gmail.com>
2020-10-23 12:27:04 -0400

    seperated some concerns

---
+++ src/components/blog-section.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react'
+
+export default function BlogSection() {
+  const [blogPosts, setBlogPosts] = useState([])
+  useEffect(() => {
+    fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@aaron_schuyler&api_key=on3y0xvh4vpwoa6kcgrjftkvm9ugrneyzpnrfpun')
+    .then(res => res.json())
+    .then((mediumData) => {
+      setBlogPosts(mediumData.items)
+      console.log(mediumData.items)
+    })
+  }, [])
+  const renderBlogPosts = () => {
+    return blogPosts.map((post) => {
+      return (
+        <article key={post.guid}>
+          <img src={post.thumbnail} />
+          <h3>{post.title}</h3>
+          <span className='meta'>{post.author} | {post.pubDate}</span>
+        </article>
+      )
+    })
+  }
+  return (
+    <div className='blog section'>
+      <h2>My Blog Posts</h2>
+      <div className='posts'>
+        {renderBlogPosts()}
+      </div>
+    </div>
+  )
+}
---
+++ src/components/skills-section.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SkillsSection() {
+  return (
+    <div className='skills section'>
+      <h2>My Skills</h2>
+    </div>
+  )
+}
