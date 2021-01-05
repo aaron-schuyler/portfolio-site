74b790a30679e526c5f5973e9286ffe720f3743d
aaron-schuyler <asky28@gmail.com>
2021-01-04 19:24:44 -0500

    fixed blog posts grid

--- src/components/portfolio-section.js
+++ src/components/portfolio-section.js
@@ -3,7 +3,8 @@ import React from 'react'
 const techProjects = [
   { title: 'Macros', description: 'Macros is a diet and fitness tracking app built primarily with Ruby on Rails.', img: '../macros-thumb.png', link: 'https://aaronschuyler-macros.herokuapp.com/days/2020-10-23', github: 'https://github.com/aaron-schuyler/macros', tech: ['Ruby on Rails', 'Javascript']},
   { title: 'Opus', description: 'Opus is a writing app with support for various rich text formatting built in vanilla Javascript and a Rails API backend.', img: '../opus-thumb.png', link: 'https://www.opus.aaronschuyler.com', github: 'https://github.com/aaron-schuyler/opus', tech: ['Ruby on Rails', 'Javascript']},
-  { title: 'Togethr', description: 'Togethr is a mutual aid request platform that is built in React with a Rails API backend, it uses JWT for authentication.', img: '../togethr-thumb.png', link: 'https://inspiring-kirch-11050f.netlify.app/tickets', github: 'https://github.com/aaron-schuyler/togethr', tech: ['React', 'Redux', 'Ruby on Rails', 'Javascript']}
+  { title: 'Togethr', description: 'Togethr is a mutual aid request platform that is built in React with a Rails API backend, it uses JWT for authentication.', img: '../togethr-thumb.png', link: 'https://inspiring-kirch-11050f.netlify.app/tickets', github: 'https://github.com/aaron-schuyler/togethr', tech: ['React', 'Redux', 'Ruby on Rails', 'Javascript']},
+  {title: 'Greenville Journal', description: 'Greenville Journal is a local news publication in South Carolina'}
 ]
 
 export default function PortfolioSection() {
@@ -15,6 +16,7 @@ export default function PortfolioSection() {
           <a className='link' href={project.link}><h3>{project.title}</h3></a>
           <p>{project.description}</p>
           {project.github && <span><a className='link' href={project.github}>Github</a> | <a className='link' href={project.link}>Demo</a></span> }
+          {!project.github && <a className='link' href={project.link}>Visit Site</a></span> }
         </article>
       )
     })
